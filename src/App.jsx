83bdd1ecd68dc3c82413e05d3ceaef7a3f94be85
4mahbubur4 <mahbubur.mr4@gmail.com>
2025-10-06 00:07:35 +0600

    bottle-data-website

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import { Suspense } from 'react'
+import './App.css'
+import Bottles from './components/Bottles/Bottles'
+
+
+const bottlesPromise = fetch('./bottle.json').then(res => res.json());
+
+
+function App() {
+ 
+  // const bottles = [
+  //   {id: 1, name: 'Pink Nike Bottle', price: 250, color: 'pink'},
+  //   {id: 2, name: 'Pink Nike Bottle', price: 250, color: 'pink'},
+  //   {id: 3, name: 'Pink Nike Bottle', price: 250, color: 'pink'},
+  //   {id: 4, name: 'Pink Nike Bottle', price: 250, color: 'pink'},
+  // ]
+
+  return (
+    <>
+      
+      <h1>Buy Awesome Water Bottle</h1>
+      <Suspense fallback={<h3>Bottles are Loading....</h3>}>
+         <Bottles bottlesPromise={bottlesPromise}></Bottles>
+      </Suspense>
+     
+     
+    </>
+  )
+}
+
+export default App
